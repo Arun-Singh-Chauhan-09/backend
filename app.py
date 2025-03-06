98bb49f8b9b5a9171411b384153cdfdfc21bb159
Ubuntu <ubuntu@ip-172-31-0-71.ap-south-1.compute.internal>
2025-03-06 10:14:24 +0000

    Initial commit - Added backend project files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="postgres.cjq4qqiagabn.ap-south-1.rds.amazonaws.com",
+        database="postgres",
+        user="postgres",
+        password="postgres"
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM mytable;')
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return str(results)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
