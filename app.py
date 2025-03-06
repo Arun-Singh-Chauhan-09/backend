c15e0fe25cffac9a950be0265575cf067d31b92f
Arun-Singh-Chauhan-09 <chauhanarunsingh09@gmail.com>
2025-03-06 18:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def index():
     conn.close()
     return str(results)
 
+@app.route('/health')
+def health():
+    return "Flask app is running!", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
